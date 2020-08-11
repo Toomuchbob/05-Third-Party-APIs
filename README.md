33674fa7a3af456978c9054c01a816560f3ab3a6
Gino Zaccardelli <ginozacc@gmail.com>
2020-08-10 23:05:38 -0400

    finished product, failed to make multiple commits during debugging

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 05-Third-Party-APIs
+
+* A Workday Scheduler that displays saved events and updates the times with color-coding based on it's relation to the current time
+
+* This project was tough only because debugging was much harder when not within the hours used on the scheduler
+
+* Below is an image of what would happen when using the scheduler outside of the hours shown, with the locally stored values showing in DevTools
+
+![Image of the Product](./assets/images/product.PNG)
--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,24 @@
-    // GIVEN I am using a daily planner to create a schedule
-    // WHEN I open the planner
-    // THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
-
 $(document).ready(function() {
+    var now = moment(); //get current moment (the current time/date)
+    var currentHour = now.hour().toString(); //set the current moment to a string
 
-    $('#current-day-time-id').text(moment().format('LLLL'));
+    $('#current-day-time-id').text(now.format('LLLL')); //display date and time on the top of the page
 
+    $('textarea').each(function() {
+        var rowIndex = parseInt($(this).closest('.row').attr('data-index')); //get the data-index of the row associated with the textarea
+        
+        $(this).val(localStorage.getItem(rowIndex.toString())); //get the locally stored value if available and show it
+
+        //change the background color of the textarea element based on it's relation to the current time
+        if(currentHour > rowIndex) {
+            $(this).addClass('bg-secondary');
+        } else if (currentHour < rowIndex) {
+            $(this).addClass('bg-info');
+        };
+    });
+    
+    $('button').on('click', function() {    //when a Save button is clicked
+        var thisIndex = $(this).closest('.row').attr('data-index'); //get the data-index of it's containing row
+        localStorage.setItem(thisIndex, $(this).closest('.row').find('textarea').val()); //and set the value of it into local storage
+    });
 });
